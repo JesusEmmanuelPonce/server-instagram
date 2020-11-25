b491e2517c64e31cfad3300e56951e181530fa9c
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-24 23:13:46 -0600

    grapql server added and queries, resolvers & schemas

---
+++ gql/schemas.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql `
+    type User{
+        id: ID
+        name: String
+        username: String 
+    }
+    type Query{
+        # User
+        getUser: User
+    }
+`
+
+module.exports = typeDefs
